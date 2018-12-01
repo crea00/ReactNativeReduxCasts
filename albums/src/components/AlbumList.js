51d87cbffff1bf9dc172ab8c07d05bf3ad5d57ed
Jerome <654321su@gmail.com>
2018-12-01 19:52:32 +0900

    #032 Lifecycle Methods

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -3,6 +3,11 @@ import { View, Text } from "react-native";
 
 // Class do not require semi-colons!
 class AlbumList extends Component {
+  // Right location to initiate some loading of data or some HTTP request to go and fetch our data and retrieve it for our application
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList');
+  }
+
   render() {
     return (
       <View>
