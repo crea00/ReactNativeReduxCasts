7dafb033cfe22051b5f9a9abb8b44f836172c9ed
Jerome <654321su@gmail.com>
2018-12-01 21:57:00 +0900

    #035 Component Level State

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,16 +1,21 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
-import axios from "axios";
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
 
 // Class do not require semi-colons!
 class AlbumList extends Component {
+  state = {
+    albums: []
+  };
   // Right location to initiate some loading of data or some HTTP request to go and fetch our data and retrieve it for our application
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      .then(response =>  this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
+  
     return (
       <View>
         <Text>Album List!!!!</Text>
