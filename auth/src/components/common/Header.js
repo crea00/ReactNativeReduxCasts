ea8ede799fb7e36b0256c999754ae41ac0e5774a
Jerome <654321su@gmail.com>
2018-12-04 15:24:31 +0900

    #076 More on Conditional Rendering

--- auth/src/components/common/Header.js
+++ auth/src/components/common/Header.js
@@ -15,7 +15,7 @@ const Header = (props) => {
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8f8',
+    backgroundColor: '#F8F8F8',
     justifyContent: 'center',
     alignItems: 'center',
     height: 60,
