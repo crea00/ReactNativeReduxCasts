0ff88af6c20e66c7c1dd1e3f3fd80fa164f17f22
Jerome <654321su@gmail.com>
2018-12-05 11:55:06 +0900

    #88 The Connect Function

---
+++ tech_stack/src/components/LibraryList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Connect } from 'react-redux';
+
+class LibraryList extends Component {
+  render() {
+    return;
+  }
+}
+ 
+export default connect()(LibraryList);
