cecd31cb38de4ace37f364b3f1dd665b19f876f1
Jerome <654321su@gmail.com>
2018-12-06 17:28:09 +0900

    #113 Typed Actions

--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,8 +1,12 @@
+import { EMAIL_CHANGED } from '../actions/types';
+
 const INITIAL_STATE = { email: '' };
 
 // The important thing to keep in mind is that we can never return 'undefined' from reducer
 export default (state=INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMAIL_CHANGED:
+      
     default:
       return state;
   }
