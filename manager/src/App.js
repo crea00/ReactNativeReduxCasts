2cd067102f2bad30fcce911bd0598cb4b4415b6e
Jerome <654321su@gmail.com>
2018-12-06 15:26:54 +0900

    #108 More on Boilerplate Setup
    - Fix an error in previous commit
    - Add firebase librar
    - Initialize Firebase in componentwillMount() in App.js

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCYbpa8Km3v3gdGeOhdSbmsvM9GeonG8V0',
+      authDomain: 'manager-cedae.firebaseapp.com',
+      databaseURL: 'https://manager-cedae.firebaseio.com',
+      projectId: 'manager-cedae',
+      storageBucket: 'manager-cedae.appspot.com',
+      messagingSenderId: '1021115286270'
+    };
+    
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello!
