cb60cf21248ad0cae2e17ec6204b83d3ff956408
Jerome <654321su@gmail.com>
2018-12-07 14:11:48 +0900

    #129 Navigating Between Routes

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { 
   EMAIL_CHANGED, 
   PASSWORD_CHANGED, 
@@ -24,7 +25,7 @@ export const passwordChanged = text => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     dispatch({ type: LOGIN_USER });
-    
+
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => loginUserSuccess(dispatch, user))
       .catch(error => {
@@ -38,11 +39,14 @@ export const loginUser = ({ email, password }) => {
 
 const loginUserFail = dispatch => {
   dispatch({ type: LOGIN_USER_FAIL })
-}
+};
 
 const loginUserSuccess = (dispatch, user) => {
   dispatch({
     type: LOGIN_USER_SUCCESS,
     payload: user
-  })
-}
+  });
+
+  Actions.employeeList();
+};
+
