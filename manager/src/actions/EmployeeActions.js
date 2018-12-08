db76e17bbbb045184b92cea86d23fce973968d6e
Jerome <654321su@gmail.com>
2018-12-08 20:40:32 +0900

    #144 Resetting Form Properties

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE, 
+  EMPLOYEE_CREATE,
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -13,6 +15,15 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
 
-  firebase.database().ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift });
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      // Update for latest version of RNRF - this should be 'Actions.pop()'
+      // to return to the previous screen, NOT Actions.employeeList()
+      // It prevents a double-scene stacking behavior!
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.pop();
+      });
+  };
 };
