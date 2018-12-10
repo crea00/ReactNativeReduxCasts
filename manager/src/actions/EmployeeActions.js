2fb1cea203dcaac3317cc2adb304c0ff21af8418
Jerome <654321su@gmail.com>
2018-12-11 01:21:54 +0900

    #145 Fetching Data from Firebase

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -3,6 +3,7 @@ import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE, 
   EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS,
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -27,3 +28,29 @@ export const employeeCreate = ({ name, phone, shift }) => {
       });
   };
 };
+
+/**
+ *  If we call employeesFetch just like one time it will immediately start up this event handler.
+ *  And for the life of the rest of our application it will call this fat arrow function
+ *  anytime, any new data comes across
+ * 
+ *  Set up to watch for some amount of new data
+ *  At any point in our applications lifecycle that there's any new vlaue that comes across, 
+ *  we will automatically dispatch an action of type EMPLOYEES_FETCH_SUCCESS with new data in there.
+ */
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    // Snapshot is not the actual data.
+    // It is an object that describes the data that we could get access to.
+      .on('value', snapshot => {
+        dispatch({
+          type: EMPLOYEES_FETCH_SUCCESS,
+          payload: snapshot.val()
+        });
+      });
+  };
+};
+
